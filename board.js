e52d2a745675cd9e70cb32da5da149f280e9f534
Jasmine Boyd <boyd.jasmine45@gmail.com>
2025-09-28 18:02:39 -0500

    updates to board add h1 to index

---
+++ board.js
@@ -0,0 +1,16 @@
+document.getElementById('homeButton').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+document.getElementById('aboutButton').addEventListener('click', function() {
+    window.location.href = 'about.html';
+});
+document.getElementById('sipButton').addEventListener('click', function() {
+    window.location.href = 'sip.html';
+});
+
+document.getElementById('contactButton').addEventListener('click', function() {
+    window.location.href = 'contact.html';
+});
+document.getElementById('boardButton').addEventListener('click', function() {
+    window.location.href = 'board.html';
+});
